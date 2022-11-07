1375d4ffbda237ae555253b52a4658c983a79e8f
rifatrakib <Rakib.1508@outlook.com>
2022-11-07 18:41:08 +0600

    added meetup components without binding data

--- meet-up/src/App.svelte
+++ meet-up/src/App.svelte
@@ -1,5 +1,39 @@
 <script>
     import Header from "./Components/Header.svelte";
+    import MeetUpItem from "./Meetups/MeetUpItem.svelte";
+
+    const meetUps = [
+        {
+            "id": "m1",
+            "title": "Coding Bootcamp",
+            "subtitle": "Learn to code in 2 hours",
+            "description": "There will be some experts teaching you how to code",
+            "imageUrl": "https://static.dnls.nl/home/5/60P9A7yn96sltKNLXJKFed/all-event-venues-of-london.jpg",
+            "address": "221B Baker St, London",
+            "contactEmail": "code@test.com",
+        },
+        {
+            "id": "m1",
+            "title": "Swim Together",
+            "subtitle": "Let's learn how to swim",
+            "description": "Having fun while teaching and swimming",
+            "imageUrl": "https://static.dnls.nl/home/5/60P9A7yn96sltKNLXJKFed/all-event-venues-of-london.jpg",
+            "address": "221B Baker St, London",
+            "contactEmail": "swim@test.com",
+        },
+    ];
 </script>
 
+<style>
+    #meetups {
+        margin-top: 5rem;
+    }
+</style>
+
 <Header />
+
+<section id="meetups">
+{#each meetUps as meetUp}
+    <MeetUpItem />
+{/each}
+</section>
