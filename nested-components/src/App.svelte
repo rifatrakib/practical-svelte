6f64f3a704adb01d39cb697501dd871523b1e4fd
rifatrakib <Rakib.1508@outlook.com>
2022-11-17 12:28:55 +0600

    custom event handlers for propagating events across components

--- nested-components/src/App.svelte
+++ nested-components/src/App.svelte
@@ -2,4 +2,7 @@
 	import Product from "./Product.svelte";
 </script>
 
-<Product productTitle="A Book" on:click={() => alert("Cliked")} />
+<Product
+    productTitle="A Book"
+    on:add-to-cart={() => alert("Added to Cart!")}
+    on:delete={() => alert("Deleted!")} />
