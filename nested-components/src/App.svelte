edd67aa82a38eeaa17ca243266eddc7572289d4f
rifatrakib <Rakib.1508@outlook.com>
2022-11-18 10:43:08 +0600

    using spread operator in props and default values

--- nested-components/src/App.svelte
+++ nested-components/src/App.svelte
@@ -1,8 +1,26 @@
 <script>
 	import Product from "./Product.svelte";
+
+    let products = [
+        {
+            id: "p1",
+            title: "A Book",
+            price: 9.99,
+        },
+    ];
+
+    function addToCart(event) {
+        console.log(event.detail);
+    }
+
+    function deleteProduct(event) {
+        console.log(event.detail);
+    }
 </script>
 
-<Product
-    productTitle="A Book"
-    on:add-to-cart={() => alert("Added to Cart!")}
-    on:delete={() => alert("Deleted!")} />
+{#each products as product}
+    <Product
+        {...product}
+        on:add-to-cart="{addToCart}"
+        on:delete="{deleteProduct}" />
+{/each}
