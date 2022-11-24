439f766d7b8f6ee1f5c64eda26b065f0a872f153
rifatrakib <Rakib.1508@outlook.com>
2022-11-24 17:43:31 +0600

    create and update lifecycle of svelte components

--- nested-components/src/Modal.svelte
+++ nested-components/src/Modal.svelte
@@ -1,9 +1,33 @@
 <script>
-    import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher, onMount, onDestroy, beforeUpdate, afterUpdate } from "svelte";
 
     const dispatch = createEventDispatcher();
 
     let agreed = false;
+    let autoscroll = false;
+
+    onMount(() => {
+        console.log("onMount");
+    });
+
+    onDestroy(() => {
+        console.log("onDestroy");
+    });
+
+    beforeUpdate(() => {
+        console.log("beforeUpdate");
+        autoscroll = agreed;
+    });
+
+    afterUpdate(() => {
+        console.log("afterUpdate");
+        if (autoscroll) {
+            const modal = document.querySelector(".modal");
+            modal.scrollTo(0, modal.scrollHeight);
+        }
+    });
+
+    console.log("script executed");
 </script>
 
 <style>
@@ -23,7 +47,7 @@
         top: 10vh;
         left: 10%;
         width: 80%;
-        max-height: 80vh;
+        max-height: 15vh;
         background: white;
         border-radius: 5px;
         z-index: 100;
