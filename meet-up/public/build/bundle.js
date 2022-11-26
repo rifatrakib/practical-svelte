878f80cecb238a8c442f99a780fca87febaa5b56
rifatrakib <Rakib.1508@outlook.com>
2022-11-26 18:50:46 +0600

    include default values to props to avoid warnings

--- meet-up/public/build/bundle.js
+++ meet-up/public/build/bundle.js
@@ -530,7 +530,7 @@ var app = (function () {
     			t = text(/*caption*/ ctx[1]);
     			attr_dev(button, "class", button_class_value = "" + (null_to_empty(/*mode*/ ctx[3]) + " svelte-9ib8ag"));
     			attr_dev(button, "type", /*type*/ ctx[0]);
-    			add_location(button, file$4, 88, 4, 1750);
+    			add_location(button, file$4, 88, 4, 1775);
     		},
     		m: function mount(target, anchor) {
     			insert_dev(target, button, anchor);
@@ -574,7 +574,7 @@ var app = (function () {
     			t = text(/*caption*/ ctx[1]);
     			attr_dev(a, "href", /*href*/ ctx[2]);
     			attr_dev(a, "class", "svelte-9ib8ag");
-    			add_location(a, file$4, 86, 4, 1705);
+    			add_location(a, file$4, 86, 4, 1730);
     		},
     		m: function mount(target, anchor) {
     			insert_dev(target, a, anchor);
@@ -661,26 +661,14 @@ var app = (function () {
     function instance$4($$self, $$props, $$invalidate) {
     	let { $$slots: slots = {}, $$scope } = $$props;
     	validate_slots('Button', slots, []);
-    	let { type } = $$props;
+    	let { type = "button" } = $$props;
     	let { caption } = $$props;
-    	let { href } = $$props;
-    	let { mode } = $$props;
+    	let { href = null } = $$props;
+    	let { mode = null } = $$props;
 
     	$$self.$$.on_mount.push(function () {
-    		if (type === undefined && !('type' in $$props || $$self.$$.bound[$$self.$$.props['type']])) {
-    			console.warn("<Button> was created without expected prop 'type'");
-    		}
-
     		if (caption === undefined && !('caption' in $$props || $$self.$$.bound[$$self.$$.props['caption']])) {
     			console.warn("<Button> was created without expected prop 'caption'");
-    		}
-
-    		if (href === undefined && !('href' in $$props || $$self.$$.bound[$$self.$$.props['href']])) {
-    			console.warn("<Button> was created without expected prop 'href'");
-    		}
-
-    		if (mode === undefined && !('mode' in $$props || $$self.$$.bound[$$self.$$.props['mode']])) {
-    			console.warn("<Button> was created without expected prop 'mode'");
     		}
     	});
 
@@ -1333,7 +1321,7 @@ var app = (function () {
     			attr_dev(input, "id", /*id*/ ctx[1]);
     			input.value = /*value*/ ctx[4];
     			attr_dev(input, "class", "svelte-19icdtr");
-    			add_location(input, file$1, 47, 8, 1011);
+    			add_location(input, file$1, 47, 8, 1034);
     		},
     		m: function mount(target, anchor) {
     			insert_dev(target, input, anchor);
@@ -1387,7 +1375,7 @@ var app = (function () {
     			attr_dev(textarea, "id", /*id*/ ctx[1]);
     			textarea.value = /*value*/ ctx[4];
     			attr_dev(textarea, "class", "svelte-19icdtr");
-    			add_location(textarea, file$1, 45, 8, 927);
+    			add_location(textarea, file$1, 45, 8, 950);
     		},
     		m: function mount(target, anchor) {
     			insert_dev(target, textarea, anchor);
@@ -1451,9 +1439,9 @@ var app = (function () {
     			if_block.c();
     			attr_dev(label_1, "for", /*id*/ ctx[1]);
     			attr_dev(label_1, "class", "svelte-19icdtr");
-    			add_location(label_1, file$1, 43, 4, 847);
+    			add_location(label_1, file$1, 43, 4, 870);
     			attr_dev(div, "class", "form-control svelte-19icdtr");
-    			add_location(div, file$1, 42, 0, 815);
+    			add_location(div, file$1, 42, 0, 838);
     		},
     		l: function claim(nodes) {
     			throw new Error("options.hydrate only works if the component was compiled with the `hydratable: true` option");
@@ -1506,18 +1494,14 @@ var app = (function () {
     function instance$1($$self, $$props, $$invalidate) {
     	let { $$slots: slots = {}, $$scope } = $$props;
     	validate_slots('TextInput', slots, []);
-    	let { controlType } = $$props;
+    	let { controlType = null } = $$props;
     	let { id } = $$props;
     	let { label } = $$props;
-    	let { rows } = $$props;
+    	let { rows = null } = $$props;
     	let { value } = $$props;
-    	let { type } = $$props;
+    	let { type = "text" } = $$props;
 
     	$$self.$$.on_mount.push(function () {
-    		if (controlType === undefined && !('controlType' in $$props || $$self.$$.bound[$$self.$$.props['controlType']])) {
-    			console.warn("<TextInput> was created without expected prop 'controlType'");
-    		}
-
     		if (id === undefined && !('id' in $$props || $$self.$$.bound[$$self.$$.props['id']])) {
     			console.warn("<TextInput> was created without expected prop 'id'");
     		}
@@ -1526,16 +1510,8 @@ var app = (function () {
     			console.warn("<TextInput> was created without expected prop 'label'");
     		}
 
-    		if (rows === undefined && !('rows' in $$props || $$self.$$.bound[$$self.$$.props['rows']])) {
-    			console.warn("<TextInput> was created without expected prop 'rows'");
-    		}
-
     		if (value === undefined && !('value' in $$props || $$self.$$.bound[$$self.$$.props['value']])) {
     			console.warn("<TextInput> was created without expected prop 'value'");
-    		}
-
-    		if (type === undefined && !('type' in $$props || $$self.$$.bound[$$self.$$.props['type']])) {
-    			console.warn("<TextInput> was created without expected prop 'type'");
     		}
     	});
 
@@ -1689,7 +1665,6 @@ var app = (function () {
     			props: {
     				id: "title",
     				label: "Title",
-    				type: "text",
     				value: /*title*/ ctx[0]
     			},
     			$$inline: true
@@ -1701,7 +1676,6 @@ var app = (function () {
     			props: {
     				id: "subtitle",
     				label: "Subtitle",
-    				type: "text",
     				value: /*subtitle*/ ctx[1]
     			},
     			$$inline: true
@@ -1713,7 +1687,6 @@ var app = (function () {
     			props: {
     				id: "address",
     				label: "Address",
-    				type: "text",
     				value: /*address*/ ctx[2]
     			},
     			$$inline: true
@@ -1725,7 +1698,6 @@ var app = (function () {
     			props: {
     				id: "imageUrl",
     				label: "Image URL",
-    				type: "text",
     				value: /*imageUrl*/ ctx[3]
     			},
     			$$inline: true
