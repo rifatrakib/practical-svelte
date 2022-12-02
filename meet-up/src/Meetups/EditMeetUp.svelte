f0d2c30395afbb4ce27c97a35a683cf9b44b53e6
rifatrakib <Rakib.1508@outlook.com>
2022-12-02 11:23:21 +0600

    emit and handle events from modal buttons

--- meet-up/src/Meetups/EditMeetUp.svelte
+++ meet-up/src/Meetups/EditMeetUp.svelte
@@ -23,6 +23,10 @@
             imageUrl: imageUrl,
         });
     }
+
+    function cancel() {
+        dispatch("cancel");
+    }
 </script>
 
 <style>
@@ -71,4 +75,8 @@
             value={description}
             on:input={event => (description = event.target.value)} />
     </form>
+    <div slot="footer">
+        <Button type="button" mode="outline" on:click="{cancel}">Cancel</Button>
+        <Button type="button" on:click="{submitForm}">Save</Button>
+    </div>
 </Modal>
