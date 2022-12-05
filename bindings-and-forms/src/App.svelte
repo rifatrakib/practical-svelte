84bfd9ea8272abf0f98f8fcd4ef5c019467dc3c2
rifatrakib <Rakib.1508@outlook.com>
2022-12-05 09:56:06 +0600

    examples of two-way bindings with custom components

--- bindings-and-forms/src/App.svelte
+++ bindings-and-forms/src/App.svelte
@@ -1,7 +1,12 @@
 <script>
+    import CustomInput from "./CustomInput.svelte";
+    import Toggle from "./Toggle.svelte";
+
     let val = "Max";
+    let selectedOption = 1;
 
     $: console.log(val);
+    $: console.log(selectedOption);
 
     function setValue(event) {
         val = event.target.value;
@@ -9,4 +14,6 @@
 </script>
 
 <!-- <input type="text" value="{val}" on:input="{setValue}" /> -->
-<input type="text" bind:value={val} />
+<!-- <input type="text" bind:value={val} /> -->
+<CustomInput bind:val={val} />
+<Toggle bind:chosenOption={selectedOption} />
