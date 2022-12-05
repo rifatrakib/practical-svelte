c94ac3bd32085bfb9eba1b7e7aa5cada0e1f0445
rifatrakib <Rakib.1508@outlook.com>
2022-12-05 09:38:33 +0600

    initialize sub-project to learn bindings and forms

---
+++ bindings-and-forms/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    let val = "Max";
+
+    $: console.log(val);
+
+    function setValue(event) {
+        val = event.target.value;
+    }
+</script>
+
+<!-- <input type="text" value="{val}" on:input="{setValue}" /> -->
+<input type="text" bind:value={val} />
