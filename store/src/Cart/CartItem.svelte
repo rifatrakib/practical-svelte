fe15a8867133b657da45850e588c81cd499d323e
rifatrakib <Rakib.1508@outlook.com>
2022-12-26 11:37:14 +0600

    combining data from multiple stores

--- store/src/Cart/CartItem.svelte
+++ store/src/Cart/CartItem.svelte
@@ -1,51 +1,61 @@
-<script>
-  import Button from "../UI/Button.svelte";
+    <script>
+    import cartItems from "./cart-store";
+    import { products } from "../Products/products-store";
+    import Button from "../UI/Button.svelte";
 
-  export let title;
-  export let price;
-  export let id;
+    export let title;
+    export let price;
+    export let id;
 
-  let showDescription = false;
+    let showDescription = false;
+    let description = "Not available!";
+    let fetchedProducts = [];
 
-  function displayDescription() {
-    showDescription = !showDescription;
-  }
+    products.subscribe(prods => {
+        fetchedProducts = prods;
+    });
 
-  function removeFromCart() {
-    // ...
-    console.log("Removing...");
-  }
+    function displayDescription() {
+        showDescription = !showDescription;
+        description = fetchedProducts.find(p => p.id === id).description;
+    }
+
+    function removeFromCart() {
+        cartItems.update(items => {
+            return items.filter(i => i.id !== id);
+        });
+    }
 </script>
 
 <style>
-  li {
-    margin: 1rem 0;
-    border-radius: 5px;
-    background: white;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    padding: 1rem;
-  }
+    li {
+        margin: 1rem 0;
+        border-radius: 5px;
+        background: white;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        padding: 1rem;
+    }
 
-  h1,
-  h2 {
-    font-size: 1rem;
-    margin: 0;
-  }
+    h1,
+    h2 {
+        font-size: 1rem;
+        margin: 0;
+    }
 
-  h2 {
-    color: #494949;
-    margin-bottom: 1rem;
-  }
+    h2 {
+        color: #494949;
+        margin-bottom: 1rem;
+    }
 </style>
 
 <li>
-  <h1>{title}</h1>
-  <h2>{price}</h2>
-  <Button mode="outline" on:click={displayDescription}>
-    {showDescription ? 'Hide Description' : 'Show Description'}
-  </Button>
-  <Button on:click={removeFromCart}>Remove from Cart</Button>
-  {#if showDescription}
-    <p>Not available :(</p>
-  {/if}
+    <h1>{title}</h1>
+    <h2>{price}</h2>
+    <Button mode="outline" on:click={displayDescription}>
+        {showDescription ? "Hide Description" : "Show Description"}
+    </Button>
+    <Button on:click={removeFromCart}>Remove from Cart</Button>
+    {#if showDescription}
+        <p>{description}</p>
+    {/if}
 </li>
