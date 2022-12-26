c46ed761b4de2ef08cfe6ee958c7a693cb389fc9
rifatrakib <Rakib.1508@outlook.com>
2022-12-26 11:55:41 +0600

    readable stores with custom and automatic sub

--- store/src/Cart/Cart.svelte
+++ store/src/Cart/Cart.svelte
@@ -1,7 +1,12 @@
 <script>
-    // import { onDestroy } from "svelte";
+    import { onDestroy } from "svelte";
+    import { timer } from "../timer-store";
     import CartItem from "./CartItem.svelte";
     import cartItems from "./cart-store.js";
+
+    const unsubscribe = timer.subscribe(count => {
+        console.log("Cart: " + count);
+    });
 
     // let items;
 
@@ -9,11 +14,11 @@
     //     items = its;
     // });
 
-    // onDestroy(() => {
-    //     if (unsubscribe) {
-    //         unsubscribe();
-    //     }
-    // });
+    onDestroy(() => {
+        if (unsubscribe) {
+            unsubscribe();
+        }
+    });
 
     // export let items = [
     //     {
