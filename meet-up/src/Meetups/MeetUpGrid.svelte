648ca6c75e5e1d0fe8ebaf173ac83bd13ed771e1
rifatrakib <Rakib.1508@outlook.com>
2022-12-28 17:17:13 +0600

    added filter with buttons having different styles

--- meet-up/src/Meetups/MeetUpGrid.svelte
+++ meet-up/src/Meetups/MeetUpGrid.svelte
@@ -1,26 +1,42 @@
 <script>
     import MeetUpItem from "./MeetUpItem.svelte";
+    import MeetUpFilter from "./MeetUpFilter.svelte";
 
     export let meetUps;
+
+    let favsOnly = false;
+
+    $: filteredMeetups = favsOnly ? meetUps.filter(m => m.isFavorite): meetUps;
+
+    function setFilter(event) {
+        favsOnly = event.detail === 1;
+    }
 </script>
 
 <style>
-    section {
+    #meetups {
         width: 100%;
         display: grid;
         grid-template-columns: 1fr;
         grid-gap: 1rem;
     }
 
+    #meetup-controls {
+        margin: 1rem;
+    }
+
     @media (min-width: 768px) {
-        section {
+        #meetups {
             grid-template-columns: repeat(2, 1fr);
         }
     }
 </style>
 
+<section id="meetup-controls">
+    <MeetUpFilter on:select={setFilter} />
+</section>
 <section id="meetups">
-    {#each meetUps as meetUp}
+    {#each filteredMeetups as meetUp}
         <MeetUpItem
             id="{meetUp.id}"
             title="{meetUp.title}"
