326d5a949dee28e802ac6cc65b0dcf77d33c0cc6
rifatrakib <Rakib.1508@outlook.com>
2022-12-31 17:06:05 +0600

    send POST request to store data in firebase

--- http/src/App.svelte
+++ http/src/App.svelte
@@ -1,5 +1,36 @@
 <script>
+    let hobbies = [];
+    let hobbyInput;
 
+    function addHobby() {
+        hobbies = [...hobbies, hobbyInput.value];
+
+        fetch(
+            "sample url for firebase",
+            {
+                method: "POST",
+                body: JSON.stringify(hobbies),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        ).then(res => {
+            if (!res.ok) {
+                throw new Error("Failed!");
+            }
+            ///
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 </script>
 
+<label for="hobby">Hobby</label>
+<input type="text" id="hobby" bind:this={hobbyInput} />
+<button on:click={addHobby}>Add Hobby</button>
 
+<ul>
+    {#each hobbies as hobby}
+        <li>hobby</li>
+    {/each}
+</ul>
