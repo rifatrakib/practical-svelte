edac29f8931684dcbe87aa7256b3edcf8ed0a4e5
rifatrakib <Rakib.1508@outlook.com>
2023-01-01 08:57:54 +0600

    save new meetup data in firebase using store

--- meet-up/src/Meetups/EditMeetUp.svelte
+++ meet-up/src/Meetups/EditMeetUp.svelte
@@ -71,7 +71,23 @@
         if (id) {
             meetups.updateMeetup(id, meetupData);
         } else {
-            meetups.addMeetup(meetupData);
+            fetch(
+                "firebase url",
+                {
+                    method: "POST",
+                    body: JSON.stringify({...meetupData, isFavorite: false}),
+                    headers: {"Content-Type": "application/json"},
+                }
+            ).then(res => {
+                if (!res.ok) {
+                    throw new Error("error occured, please try again");
+                }
+                return res.json();
+            }).then(data => {
+                meetups.addMeetup({...meetupData, isFavorite: false, id: data.name});
+            }).catch(err => {
+                console.log(err);
+            });
         }
         dispatch("save");
     }
