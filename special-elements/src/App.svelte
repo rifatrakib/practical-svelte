c0316b4746998ad4daf2fd8006d69b6eb78fd559
rifatrakib <Rakib.1508@outlook.com>
2023-01-06 09:52:30 +0600

    recursive svelte component to output component inside itself

--- special-elements/src/App.svelte
+++ special-elements/src/App.svelte
@@ -1,6 +1,27 @@
 <script>
     import Product from "./Product.svelte";
     import CartItem from "./CartItem.svelte";
+    import FamilyNode from "./FamilyNode.svelte";
+
+    let familyStructure = [
+        {
+            isParent: true,
+            name: "Chris",
+            children: [
+                {
+                    isParent: true,
+                    name: "Moe",
+                    children: [
+                        {
+                            isParent: false,
+                            name: "Julie",
+                        },
+                    ],
+                },
+            ],
+        },
+        {isParent: false, name: "Anna"},
+    ];
 
     let renderedItem = {cmp: CartItem, title: "Another Product", id: "p2"};
 
@@ -16,3 +37,7 @@
 <button on:click={toggle}>Toggle Display</button>
 
 <svelte:component this={renderedItem.cmp} title="{renderedItem.title}" id="{renderedItem.id}" />
+
+{#each familyStructure as familyMember}
+    <FamilyNode member={familyMember} />
+{/each}
