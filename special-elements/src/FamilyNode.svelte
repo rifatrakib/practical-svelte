5800b9882bce3e2023d56df18f3c712a8e8fb39f
rifatrakib <Rakib.1508@outlook.com>
2023-01-09 08:12:45 +0600

    share data between same type of components

--- special-elements/src/FamilyNode.svelte
+++ special-elements/src/FamilyNode.svelte
@@ -1,14 +1,41 @@
+<script context="module">
+    console.log("runs once!");
+
+    let deactivateNode;
+</script>
+
 <script>
     export let member;
+
+    let isActive;
+
+    console.log("runs multiple times!");
+
+    function deactivate() {
+        isActive = false;
+    }
+
+    function activate() {
+        if (deactivateNode) {
+            deactivateNode();
+        }
+        isActive = true;
+        deactivateNode = deactivate;
+    }
 </script>
 
 <style>
     div {
         margin-left: 2rem;
     }
+
+    .active {
+        color: red;
+    }
 </style>
 
-<div>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div on:click={activate} class:active={isActive}>
     <h1>{member.name}</h1>
     {#if member.isParent}
         {#each member.children as child}
